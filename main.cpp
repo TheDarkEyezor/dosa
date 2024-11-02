9660feef14546b8d5bb3a1a80fcdccfc1ee31c62
Adi Prabs <aditya.prabakaran@gmail.com>
2024-11-02 15:58:30 +0000

    Multithreading added for alarm to be active while main program still runs

--- main.cpp
+++ main.cpp
@@ -6,20 +6,19 @@
 
 
 // Function to parse command and extract hour and minute
-Command *parse_command(const std::string &command) {
-    alarm emptyAlarm;
+Command* parse_command(const std::string& command) {
     std::istringstream stream(command);
     std::string temp;
-    
+
     // Parsing "set an alarm for HH:MM today"
     while (stream >> temp) {
         if (temp.find(':') != std::string::npos) {
             size_t colon_pos = temp.find(':');
             int hour = std::stoi(temp.substr(0, colon_pos));
             int minute = std::stoi(temp.substr(colon_pos + 1));
-            alarm newAlarm;
-            newAlarm.init(hour, minute);
-            return &newAlarm;
+            alarm* newAlarm = new alarm();
+            newAlarm->init(hour, minute);
+            return newAlarm;
         }
     }
     return nullptr;
@@ -30,14 +29,12 @@ int main() {
     std::cout << "Enter your command: ";
     std::getline(std::cin, command);
 
-    Command *bruh = parse_command(command);
+    Command *newCommand = parse_command(command);
     // // Start alarm in a separate thread
-    // std::thread alarm_thread([bruh]() { bruh->execute(); });
+    std::thread alarm_thread(&Command::execute, newCommand);
 
     // // Detach the alarm thread so it runs independently
-    // alarm_thread.detach();
-
-    bruh->execute();
+    alarm_thread.detach();
 
     // Main thread can do other tasks here
     std::cout << "Alarm is set. Main program is free to do other tasks." << std::endl;
